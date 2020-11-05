1dca104d26a34b412b9d3042af7262f0487a08c4
Udjin Stolboviy <UdjinStolbovy@gmail.com>
2020-11-05 21:27:22 +0200

    build-prod-js end

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,7 +3,8 @@ const modals = () => {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
             close = document.querySelector(closeSelector),
-            windows = document.querySelectorAll('[data-modal]');
+            windows = document.querySelectorAll('[data-modal]'),
+            scroll = calcScroll();
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
@@ -16,7 +17,12 @@ const modals = () => {
                 });
 
                 modal.style.display = "block";
-                document.body.style.overflow = "hidden"; // Отменяет  прокрутку  страници
+                document.body.style.overflow = "hidden";
+                 // Отменяет  прокрутку  страници
+                document.body.style.marginRight = `${scroll}px`;
+                 
+
+
                 // document.body.classList.add('modal-open');
             });
         });
@@ -29,6 +35,8 @@ const modals = () => {
             modal.style.display = "none";
             document.body.style.overflow = "";
             // document.body.classList.remove('modal-open');
+            document.body.style.marginRight = `0px`;
+            
         });
 
         modal.addEventListener('click', (e) => {
@@ -40,6 +48,7 @@ const modals = () => {
                 modal.style.display = "none";
                 document.body.style.overflow = "";
                 // document.body.classList.remove('modal-open');
+                document.body.style.marginRight = `0px`;
             }
         });
     }
@@ -49,6 +58,21 @@ const modals = () => {
             document.querySelector(selector).style.display = 'block';
             document.body.style.overflow = "hidden";
         }, time);
+    }
+
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflow = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
     }
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
